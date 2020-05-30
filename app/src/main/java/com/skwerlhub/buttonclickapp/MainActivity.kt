d8af250ce8e6f09cfc23cbcc0c9ae355643b0101
gv4383 <gregory.vargas18@gmail.com>
2020-05-30 15:51:03 -0500

    Add onClick listener

--- app/src/main/java/com/skwerlhub/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/skwerlhub/buttonclickapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.skwerlhub.buttonclickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,13 +11,21 @@ class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
     private var button: Button? = null
     private var textView: TextView? = null
+    private var numTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.button)
+        userInput = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
+
+        button?.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                numTimesClicked += 1
+                textView?.append("\nThe button got tapped $numTimesClicked time")
+            }
+        })
     }
 }
